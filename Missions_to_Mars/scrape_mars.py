d8c08328ca4d2ad096ab923c69d23905f7cf4b9f
Antler_Zutter <yycyyc.happy@gmail.com>
2021-08-02 19:38:31 +1000

    change code

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -50,61 +50,75 @@ def scrape_all():
     #connect to Mars Facts
     #Scraping from that browser
 
-    url_facts = "https://galaxyfacts-mars.com/"
-    browser.visit(url_facts)
+    mars_facts = pd.read_html("https://space-facts.com/mars/")[1]
+    mars_facts.set_index(['Mars - Earth Comparison'])
 
-    #start to scrap the table from Mars Facts
+    #connet to cerberus hemispheres
+    url = "https://marshemispheres.com/cerberus.html"
+    browser.visit(url)
+ 
+    #start to scrap the name of cerberus
     html = browser.html
     soup = bs(html, "html.parser")
-    table_content = soup.select_one("table", class_="table table-striped")
+    cerberus = soup.find("h2", class_="title").get_text()
+    
+    #start to scrap the html of cerberus
+    content = browser.find_link_by_text("Sample").first
+    cerberus_link = content['href']
+    
+    cerberus_hem = {"title": cerberus, "img_url": cerberus_link}
 
-    #create the list for table
-    index = []
-    mars_list = []
-    earth_list = []
+    #connet to schiaparelli hemispheres
+    url = "https://marshemispheres.com/schiaparelli.html"
+    browser.visit(url)
 
-    categories = table_content.find_all('tr')
-
-    for category in categories:
-        index.append(category.find('th').get_text())
-        mars_list.append(category.find_all('td')[0].get_text())
-        earth_list.append(category.find_all('td')[1].get_text())
-
-    #put the table into panda and do some adjustment
-    df = pd.DataFrame(list(zip(index,mars_list,earth_list)))
-    df = df.set_index(0)
-    df.columns = ['Mars', 'Earth']
-    df.index.name = 'Mars-Earth Comparison'
-    df = df.iloc[1:,:]
-    df.loc['Temperature:','Earth'] = '-88 to 58°C'
-
-    #create the mars_facts html code for table
-    mars_facts = df.to_html()
-
-    #connect to Mars Hemisphere
-    url_hemi = "https://marshemispheres.com"
-    browser.visit(url_hemi)
-
-    #start to scrap the table from Mars Facts
+    #start to scrap the name of schiaparelli
     html = browser.html
     soup = bs(html, "html.parser")
-    hemis = soup.find("div", class_="collapsible results")
+    schiaparelli = soup.find("h2", class_="title").get_text()
+    
+    #start to scrap the html of schiaparelli
+    content = browser.find_link_by_text("Sample").first
+    schiaparelli_link = content['href']
+    
+    schiaparelli_hem = {"title": schiaparelli, "img_url": schiaparelli_link}
 
-    #get a list of how many pictures contains inside
-    categories = hemis.find_all('div', class_= 'item') 
+    #connet to syrtis major hemispheres
+    url = "https://marshemispheres.com/syrtis.html"
+    browser.visit(url)
 
+    #start to scrap the name of syrtis major
+    html = browser.html
+    soup = bs(html, "html.parser")
+    syrtis = soup.find("h2", class_="title").get_text()
+    
+    #start to scrap the html of syrtis major
+    content = browser.find_link_by_text("Sample").first
+    syrtis_link = content['href']
+    
+    syrtis_hem = {"title": syrtis, "img_url": syrtis_link}
+
+    #connet to syrtis valles hemispheres
+    url = "https://marshemispheres.com/valles.html"
+    browser.visit(url)
+
+    #start to scrap the name of valles major
+    html = browser.html
+    soup = bs(html, "html.parser")
+    valles = soup.find("h2", class_="title").get_text()
+
+    #start to scrap the html of valles
+    content = browser.find_link_by_text("Sample").first
+    valles_link = content['href']
+
+    valles_hem = {"title": valles, "img_url": valles_link}
     hemisphere_image_urls = []
+    hemisphere_image_urls.append(cerberus_hem)
+    hemisphere_image_urls.append(schiaparelli_hem)
+    hemisphere_image_urls.append(syrtis_hem)
+    hemisphere_image_urls.append(valles_hem)
 
-    for category in categories:
-        hemisphere = {}
-    
-        #find image url and title and put into dictionary
-        image_src = category.find("img",class_="thumb")['src']
-        hemisphere['title'] = category.find('h3').get_text()
-        hemisphere['img_url'] = f'https://marshemispheres.com/{image_src}'
-    
-        #put the hemisphere into a list
-        hemisphere_image_urls.append(hemisphere)
+
     
     listing['first_title'] = first_title
     listing['first_para'] = first_para
